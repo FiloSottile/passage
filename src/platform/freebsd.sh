96836e447afac427a5cdd87534146418146dc8f9
Jason A. Donenfeld <Jason@zx2c4.com>
2012-09-23 09:26:09 +0200

    Fix style.

--- src/platform/freebsd.sh
+++ src/platform/freebsd.sh
@@ -1,18 +1,16 @@
+# Copyright (C) 2012 Jonathan Chu <milki@rescomp.berkeley.edu>. All Rights Reserved.
+# This file is licensed under the GPLv2+. Please see COPYING for more information.
+
 tmpdir() {
-    ramdisk="/var/tmp/password-store.ramdisk"
-    if [[ -d $ramdisk && -d $ramdisk && -d $ramdisk ]]; then
+	ramdisk="/var/tmp/password-store.ramdisk"
+	if [[ -d $ramdisk && -d $ramdisk && -d $ramdisk ]]; then
 		tmp_dir="$(TMPDIR=$ramdisk mktemp -t "$template" -d)"
 	else
-		yesno "$(cat <<PROMPT
-A ramdisk does not exist at
+		yesno "$(echo    "A ramdisk does not exist at $ramdisk, which means that it may"
+			 echo    "be difficult to entirely erase the temporary non-encrypted"
+			 echo    "password file after editing. Are you sure you would like to"
+			 echo -n "continue?")"
 
-    $ramdisk
-
-which means that it may be difficult to entirely erase
-the temporary non-encrypted password file after editing.
-Are you sure you would like to continue?
-PROMPT
-)"
 		tmp_dir="$(mktemp -t "$template" -d)"
 	fi
 }
