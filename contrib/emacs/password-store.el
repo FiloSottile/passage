7d74e9d32c78fdfe9c02ba2b5b86b6b9bb3c8b88
Svend Sorensen <svend@ciffer.net>
2014-06-16 11:41:05 -0700

    emacs: Update pass website URL

--- contrib/emacs/password-store.el
+++ contrib/emacs/password-store.el
@@ -27,7 +27,7 @@
 ;; This package provides functions for working with pass ("the
 ;; standard Unix password manager").
 ;;
-;; http://www.zx2c4.com/projects/password-store/
+;; http://www.passwordstore.org/
 
 ;;; Code:
 
