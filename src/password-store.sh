c34d172ec14b06de82c04eecf86af66cd26379d6
Lenz Weber <mail@lenzw.de>
2014-09-21 21:31:05 +0200

    tree listing: remove .gpg at the end, but keep bash colorzing

--- src/password-store.sh
+++ src/password-store.sh
@@ -332,7 +332,7 @@ cmd_show() {
 		else
 			echo "${path%\/}"
 		fi
-		tree -C -l --noreport "$PREFIX/$path" | tail -n +2 | sed 's/\.gpg$//'
+                tree -C -l --noreport "$PREFIX/$path" | tail -n +2 | sed 's/\.gpg\(\x1B\[[0-9]\+m\)\{0,1\}$/\1/' # remove .gpg at end of line, but keep colors
 	elif [[ -z $path ]]; then
 		die "Error: password store is empty. Try \"pass init\"."
 	else
