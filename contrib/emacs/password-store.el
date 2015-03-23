e0deb90cc08187eb7e9019846d452c5a4e9c083a
Svend Sorensen <svend@ciffer.net>
2015-03-22 23:48:24 -0700

    emacs: Use spaces for indentation

--- contrib/emacs/password-store.el
+++ contrib/emacs/password-store.el
@@ -55,18 +55,18 @@ Nil arguments are ignored.  Returns the output on success, or
 outputs error message on failure."
   (with-temp-buffer
     (let* ((tempfile (make-temp-file ""))
-	   (exit-code
-	    (apply 'call-process
-		   (append
-		    (list password-store-executable nil (list t tempfile) nil)
-		    (-reject 'null args)))))
+           (exit-code
+            (apply 'call-process
+                   (append
+                    (list password-store-executable nil (list t tempfile) nil)
+                    (-reject 'null args)))))
       (unless (zerop exit-code)
-	(erase-buffer)
-	(insert-file-contents tempfile))
+        (erase-buffer)
+        (insert-file-contents tempfile))
       (delete-file tempfile)
       (if (zerop exit-code)
-	  (s-chomp (buffer-string))
-	(error (s-chomp (buffer-string)))))))
+          (s-chomp (buffer-string))
+        (error (s-chomp (buffer-string)))))))
 
 (defun password-store--run-init (gpg-ids &optional folder)
   (apply 'password-store--run "init"
@@ -149,8 +149,8 @@ outputs error message on failure."
   (unless subdir (setq subdir ""))
   (let ((dir (f-join (password-store-dir) subdir)))
     (if (f-directory? dir)
-	(mapcar 'password-store--file-to-entry
-		(f-files dir (lambda (file) (equal (f-ext file) "gpg")) t)))))
+        (mapcar 'password-store--file-to-entry
+                (f-files dir (lambda (file) (equal (f-ext file) "gpg")) t)))))
 
 ;;;###autoload
 (defun password-store-edit (entry)
@@ -204,11 +204,11 @@ Separate multiple IDs with spaces."
 (defun password-store-insert (entry password)
   "Insert a new ENTRY containing PASSWORD."
   (interactive (list (read-string "Password entry: ")
-		     (read-passwd "Password: " t)))
+                     (read-passwd "Password: " t)))
   (message (shell-command-to-string (format "echo %s | %s insert -m -f %s"
-					    (shell-quote-argument password)
-					    password-store-executable
-					    (shell-quote-argument entry)))))
+                                            (shell-quote-argument password)
+                                            password-store-executable
+                                            (shell-quote-argument entry)))))
 
 ;;;###autoload
 (defun password-store-generate (entry &optional password-length)
@@ -216,8 +216,8 @@ Separate multiple IDs with spaces."
 
 Default PASSWORD-LENGTH is `password-store-password-length'."
   (interactive (list (read-string "Password entry: ")
-		     (when current-prefix-arg
-		       (abs (prefix-numeric-value current-prefix-arg)))))
+                     (when current-prefix-arg
+                       (abs (prefix-numeric-value current-prefix-arg)))))
   (unless password-length (setq password-length password-store-password-length))
   ;; A message with the output of the command is not printed because
   ;; the output contains the password.
@@ -252,8 +252,8 @@ avoid sending a password to the browser."
   (interactive (list (password-store--completing-read)))
   (let ((url (password-store-get entry)))
     (if (or (string-prefix-p "http://" url)
-	    (string-prefix-p "https://" url))
-	(browse-url url)
+            (string-prefix-p "https://" url))
+        (browse-url url)
       (error "%s" "String does not look like a URL"))))
 
 (provide 'password-store)
