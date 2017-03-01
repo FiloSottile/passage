6c7425b16895c6925c4f1ec875dac4576574b34c
Jason A. Donenfeld <Jason@zx2c4.com>
2017-03-01 14:35:11 -0800

    tests: fix on OSX by not using the tr hack

--- tests/t0010-generate-tests.sh
+++ tests/t0010-generate-tests.sh
@@ -12,8 +12,8 @@ test_expect_success 'Test "generate" command' '
 
 test_expect_success 'Test replacement of first line' '
 	"$PASS" insert -m cred2 <<<"$(printf "this is a big\\npassword\\nwith\\nmany\\nlines\\nin it bla bla")" &&
-	PATH="$TEST_HOME:$PATH" FAKE_PWGEN_PASSWORD="This is a fake password" "$PASS" generate -i cred2 23 &&
-	[[ $("$PASS" show cred2) == "$(printf "This is a fake password\\npassword\\nwith\\nmany\\nlines\\nin it bla bla")" ]]
+	"$PASS" generate -i cred2 23 &&
+	[[ $("$PASS" show cred2) == "$(printf "%s\\npassword\\nwith\\nmany\\nlines\\nin it bla bla" "$("$PASS" show cred2 | head -n 1)")" ]]
 '
 
 test_done
