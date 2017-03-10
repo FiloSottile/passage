6f867674272a03273212259b9039c9356a6ed90f
Andreas Stieger <astieger@suse.com>
2017-03-10 15:43:26 +0100

    Fix compatibility with GnuPG 2.2.19
    GnuPG 2.2.19 added a warning when no command was given.
    
    * src/password-store.sh (reencrypt_path): Add --decrypt to --list-only
    * tests/t0300-reencryption.sh (gpg_keys_from_encrypted_file): same
    
    https://bugs.gnupg.org/gnupg/msg9873
    http://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=commit;h=810adfd47801fc01e45fb71af9f05c91f7890cdb
    https://bugzilla.suse.com/show_bug.cgi?id=1028867

--- src/password-store.sh
+++ src/password-store.sh
@@ -125,7 +125,7 @@ reencrypt_path() {
 			done
 			gpg_keys="$($GPG $PASSWORD_STORE_GPG_OPTS --list-keys --with-colons "${GPG_RECIPIENTS[@]}" | sed -n 's/sub:[^:]*:[^:]*:[^:]*:\([^:]*\):[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[a-zA-Z]*e[a-zA-Z]*:.*/\1/p' | LC_ALL=C sort -u)"
 		fi
-		current_keys="$($GPG $PASSWORD_STORE_GPG_OPTS -v --no-secmem-warning --no-permission-warning --list-only --keyid-format long "$passfile" 2>&1 | cut -d ' ' -f 5 | LC_ALL=C sort -u)"
+		current_keys="$($GPG $PASSWORD_STORE_GPG_OPTS -v --no-secmem-warning --no-permission-warning --decrypt --list-only --keyid-format long "$passfile" 2>&1 | cut -d ' ' -f 5 | LC_ALL=C sort -u)"
 
 		if [[ $gpg_keys != "$current_keys" ]]; then
 			echo "$passfile_display: reencrypting to ${gpg_keys//$'\n'/ }"
